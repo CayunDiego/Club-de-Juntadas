c4c21a011b2703756d66d3c88db3b0cb7bb4e630
Diego Cayún <dcayun@aivo.co>
2024-11-11 18:35:32 -0300

    new style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,34 @@
 /* src/components/Layout.js */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Layout.scss';
 import logo from '../assets/club_de_juntadas_logo.png';
 
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+
   return (
     <div className="layout">
       <header className="header">
         <div className='logo-container'>
-          <img src={logo} alt="Logo de la Marca" className="logo" />
+          <Link to="/Club-de-Juntadas/">
+            <img src={logo} alt="Logo de la Marca" className="logo" />
+          </Link>
         </div>
         <nav className="navbar">
-          <Link to="/Club-de-Juntadas/">Home</Link>
-          <Link to="/Club-de-Juntadas/designs">Diseños</Link>
+          <Link 
+            to="/Club-de-Juntadas/" 
+            className={location.pathname === '/Club-de-Juntadas/' ? 'active' : ''}
+          >
+            Home
+          </Link>
+          <Link 
+            to="/Club-de-Juntadas/designs" 
+            className={location.pathname === '/Club-de-Juntadas/designs' ? 'active' : ''}
+          >
+            Diseños
+          </Link>
         </nav>
       </header>
       <main className='main-container'>{children}</main>
